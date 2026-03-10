3753ea8651812f69ac5b69402bdf12f04ae368a4
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-03-10 17:25:00 +0100

    add post method for task-1

---
+++ task-1/post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+response=$(curl -X POST http://localhost:3000/users \
+  -H "Content-Type: application/json; charset=UTF-8" \
+  -d '{"name":"John Doe", "email": "john.doe@example.com", "password": "secret123", "role": "user", "active": true, "department": "Engineering"}')
+
+  echo "Response from server"
+  echo "$response"
+
+  id_line=$(echo "$response" | grep '"id"') 
+  
+  id=$(echo "$id_line" | tr -cd '0-9') 
+  
+  echo "$id" > task-1/id.txt 
+  
+  echo "Saved id: $id"
+
